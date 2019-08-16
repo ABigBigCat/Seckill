b16d742c6d0471e18de9311d0a54da40dc2f52af
ABigBigCat <550571860@qq.com>
2019-08-16 10:05:54 +0800

    注册接口

--- src/main/java/com/miaoshaproject/controller/UserController.java
+++ src/main/java/com/miaoshaproject/controller/UserController.java
@@ -1,22 +1,81 @@
 package com.miaoshaproject.controller;
 
+import com.alibaba.druid.util.StringUtils;
 import com.miaoshaproject.controller.viewobject.UserVO;
 import com.miaoshaproject.error.BusinessException;
+import com.miaoshaproject.error.EmBussinessError;
 import com.miaoshaproject.response.CommonReturnType;
 import com.miaoshaproject.service.UserService;
 import com.miaoshaproject.service.model.UserModel;
+import org.apache.tomcat.util.security.MD5Encoder;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Random;
+
 
 @Controller("user")
 @RequestMapping("/user")
+@CrossOrigin
 public class UserController extends BaseController{
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    //用户注册接口
+    @RequestMapping(value = "/register",method = {RequestMethod.POST},consumes = {CONTENT_TYPE_FORMED})
+    @ResponseBody
+    private CommonReturnType register(@RequestParam(name = "telphone")String telphone,
+                                      @RequestParam(name = "otpCode")String otpCode,
+                                      @RequestParam(name = "name")String name,
+                                      @RequestParam(name = "age")Integer age,
+                                      @RequestParam(name = "gender")Byte gender,
+                                      @RequestParam(name = "password")String password) throws BusinessException {
+        //验证手机号和otpcode符合
+        String inSessionOtpCode = (String) this.httpServletRequest.getSession().getAttribute(telphone);
+        if (!StringUtils.equals(otpCode,inSessionOtpCode)){
+            throw new BusinessException(EmBussinessError.PARAMETER_VALIDATION_ERROR,"短信验证码不符合");
+        }
+
+        //用户注册流程
+        UserModel userModel = new UserModel();
+        userModel.setName(name);
+        userModel.setAge(age);
+        userModel.setGender(gender);
+        userModel.setTelphone(telphone);
+        userModel.setRegisterModel("byphone");
+        userModel.setEncrptPassword(MD5Encoder.encode(password.getBytes()));
+
+        userService.register(userModel);
+
+
+        return CommonReturnType.create(null);
+    }
+
+    //用户获取Otp短信接口
+    @RequestMapping(value = "/getotp",method = {RequestMethod.POST},consumes = {CONTENT_TYPE_FORMED})
+    @ResponseBody
+    private CommonReturnType getOtp(@RequestParam(name = "telphone")String telphone){
+        //按照一定规则生成otp验证码
+        Random random = new Random();
+        int randomInt = random.nextInt(99999);
+        randomInt += 10000;
+        String otpCode = String.valueOf(randomInt);
+
+        //将otp验证码和用户手机号关联，使用httpsession的方式绑定用户手机号和otpcode
+        httpServletRequest.getSession().setAttribute(telphone,otpCode);
+
+        //将otp验证码通过短信通道发送到用户
+        System.out.println("telphone = " + telphone + "& otpCode = " + otpCode);
+
+        return CommonReturnType.create(null);
+    }
 
     @RequestMapping("/get")
     @ResponseBody
--- src/main/java/com/miaoshaproject/service/impl/UserServiceImpl.java
+++ src/main/java/com/miaoshaproject/service/impl/UserServiceImpl.java
@@ -4,11 +4,15 @@ import com.miaoshaproject.dao.UserDOMapper;
 import com.miaoshaproject.dao.UserPasswordDOMapper;
 import com.miaoshaproject.dataobject.UserDO;
 import com.miaoshaproject.dataobject.UserPasswordDO;
+import com.miaoshaproject.error.BusinessException;
+import com.miaoshaproject.error.EmBussinessError;
 import com.miaoshaproject.service.UserService;
 import com.miaoshaproject.service.model.UserModel;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -20,6 +24,10 @@ public class UserServiceImpl implements UserService {
     private UserPasswordDOMapper userPasswordDOMapper;
 
 
+    /**
+     * 获取用户信息
+     * Created by 刘家辉 on 2019/8/15 17:18
+     */
     @Override
     public UserModel getUserById(Integer id) {
         //调用userdomapper获取对用的用户信息
@@ -33,6 +41,57 @@ public class UserServiceImpl implements UserService {
         UserPasswordDO userPasswordDO = userPasswordDOMapper.selectByUserId(userDO.getId());
 
         return convertFromDateObject(userDO,userPasswordDO);
+
+    }
+
+    /**
+     * 用户注册实现
+     * Created by 刘家辉 on 2019/8/15 17:13
+     */
+    @Override
+    @Transactional
+    public void register(UserModel userModel) throws BusinessException {
+        if (userModel == null){
+            throw new BusinessException(EmBussinessError.PARAMETER_VALIDATION_ERROR);
+        }
+        if (StringUtils.isEmpty(userModel.getName())
+            || userModel.getAge() == null
+            || userModel.getGender() == null
+            || StringUtils.isEmpty(userModel.getTelphone())){
+            throw new BusinessException(EmBussinessError.PARAMETER_VALIDATION_ERROR);
+        }
+
+        //实现model-->dataobject
+        UserDO userDO = converFromModel(userModel);
+
+        userDOMapper.insertSelective(userDO);
+
+        UserPasswordDO userPasswordDO = converPassWordFromModel(userModel);
+
+        userPasswordDOMapper.insertSelective(userPasswordDO);
+
+
+    }
+
+    private UserPasswordDO converPassWordFromModel(UserModel userModel){
+        if (userModel == null){
+            return null;
+        }
+
+        UserPasswordDO userPasswordDO = new UserPasswordDO();
+        userPasswordDO.setEncrptPassword(userModel.getEncrptPassword());
+        userPasswordDO.setUserId(userModel.getId());
+        return userPasswordDO;
+    }
+
+    private UserDO converFromModel(UserModel userModel){
+
+        if (userModel == null){
+            return null;
+        }
+        UserDO userDO = new UserDO();
+        BeanUtils.copyProperties(userModel,userDO);
+        return userDO;
 
     }
 
